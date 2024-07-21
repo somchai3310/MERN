52868749554f1e8b40e82f0b6c5098b274d8c946
somchai3310 <somchaibefore@gmail.com>
2024-07-21 20:27:36 +0700

    - add upload product - add list product category - add list all products

--- frontend/src/pages/AllProducts.js
+++ frontend/src/pages/AllProducts.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import UploadProduct from "../components/UploadProduct";
 
 const AllProducts = () => {
+  const [openUploadProduct, setOpenUploadProduct] = useState(false);
+  console.log('openUploadProduct',openUploadProduct);
   return (
-    <div>AllProducts</div>
-  )
-}
+    <div>
+      <div className="bg-white py-4 px-2 flex justify-between items-center">
+        <h2 className="font-bold text-lg">All Products</h2>
+        <button className="border-2 border-red-600 text-red-600 hover:bg-red-600 hover:text-white transition-all py-1 px-3 rounded-full"
+        onClick={()=> setOpenUploadProduct(true)}>
+          Upload Product
+        </button>
+      </div>
 
-export default AllProducts
+      {/* upload product component */}
+      {openUploadProduct && <UploadProduct onClose={()=> setOpenUploadProduct(false)} />}
+    </div>
+  );
+};
+
+export default AllProducts;
