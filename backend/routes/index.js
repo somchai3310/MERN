cf38e842a796103473f9ac283cdf0ec2cc1d1926
somchai3310 <somchaibefore@gmail.com>
2024-07-26 21:32:30 +0700

    - get category list product - get product details

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -13,6 +13,8 @@ const UploadProductController = require("../controller/product/uploadProduct");
 const getProductController = require("../controller/product/getProduct");
 const updateProductController = require("../controller/product/updateProduct");
 const getCategoryProduct = require("../controller/product/getCategoryProduct");
+const getCategoryWiseProduct = require("../controller/product/getCategoryWiseProduct");
+const getProductDetails = require("../controller/product/getProductDetails");
 
 router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
@@ -28,5 +30,7 @@ router.post("/upload-product", authToken, UploadProductController);
 router.get("/get-product", getProductController);
 router.post("/update-product", authToken, updateProductController);
 router.get("/get-category-product", getCategoryProduct)
+router.post('/category-product', getCategoryWiseProduct)
+router.post('/product-details', getProductDetails)
 
 module.exports = router;
