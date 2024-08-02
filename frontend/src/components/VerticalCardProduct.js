fdff1ac6f4051990b7cb894d392d608099995649
somchai3310 <somchaibefore@gmail.com>
2024-08-03 02:19:32 +0700

    fixbug key Vetical card

--- frontend/src/components/VerticalCardProduct.js
+++ frontend/src/components/VerticalCardProduct.js
@@ -52,7 +52,7 @@ const VerticalCardProduct = ({ category, heading }) => {
         {loading
           ? loadingList.map((product, index) => {
               return (
-                <div className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
+                <div key={`loading-card-horizon-${index}`} className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
                   <div className="bg-slate-300 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse"></div>
                   <div className="p-4 grid gap-3 animate-pulse">
                     <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 bg-slate-300 rounded-full">
@@ -70,7 +70,7 @@ const VerticalCardProduct = ({ category, heading }) => {
             })
           : data.map((product, index) => {
               return (
-                <Link to={`product/${product?._id}`} className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
+                <Link to={`product/${product?._id}`} key={product?._id} className="w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow">
                   <div className="bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center">
                     <img
                       src={product?.productImage[0]}
